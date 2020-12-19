0ee2d388316fc254caeaa6f9d6172ecbaf7e0bcc
Zoe <hi@zoe.im>
2020-12-19 19:57:44 +0800

    :tada: use micromatch to replace custom regex

--- src/helper/page-wrapper.js
+++ src/helper/page-wrapper.js
@@ -1,7 +1,9 @@
 
 const pageWrapHelper = (rules = [], path) => {
     // TODO: improve *
-    let p = rules.filter((e) => e.path === "*" || e.path === path || new RegExp(e.path).test(path))
+    // use micromatch to match
+    // let p = rules.filter((e) => e.path === "*" || e.path === path || new RegExp(e.path).test(path))
+    let p = rules.filter((e) => micromatch.isMatch(path, e.path))
     return p.length > 0 ? p[0] : null
 }
 
