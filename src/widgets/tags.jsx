44276dd0584295b43bd6c70421fae3057e61bbc7
Zoe <hi@zoe.im>
2021-08-12 20:22:43 +0800

    :tada: beauty

--- src/widgets/tags.jsx
+++ src/widgets/tags.jsx
@@ -1,18 +1,21 @@
-import { Box, Flex, HStack, Link, Tag, TagLabel, TagLeftIcon } from '@chakra-ui/react'
+import { Box, Flex, HStack, Link, Tag, TagLabel, TagLeftIcon, Wrap, WrapItem } from '@chakra-ui/react'
 import kebabCase from 'lodash.kebabcase'
 import React from 'react'
 import MLink from '../components/link'
 import { genColor } from '../styles/colors'
 
 const Tags = ({ items = [], simple = false, counted = false, ...props }) => {
-    return <Flex flexWrap="wrap" spaceing={4} {...props}>{items.map(({name, totalCount}, idx) => (
-    <Box pr="2" pb="4" key={idx}>
-        <MLink pure href={`/blogs/tag/${kebabCase(name)}`}>
-            <Tag borderRadius="full" colorScheme={simple?"gray":genColor(name.slice(0).charCodeAt())}>
-                <TagLabel># {name} {counted?`(${totalCount})`:""}</TagLabel>
-            </Tag>
-        </MLink>
-    </Box>))}</Flex>
+    return <Wrap spacing={3} {...props}>{items.map(({name, totalCount}, idx) => (
+        <WrapItem key={idx}>
+            <Box>
+                <MLink pure href={`/blogs/tag/${kebabCase(name)}`}>
+                    <Tag borderRadius="full" colorScheme={simple?"gray":genColor(name.slice(0).charCodeAt())}>
+                        <TagLabel># {name} {counted?`(${totalCount})`:""}</TagLabel>
+                    </Tag>
+                </MLink>
+            </Box>
+        </WrapItem>))}
+    </Wrap>
 }
 
 export default Tags
