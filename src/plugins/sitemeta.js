0dda02c8406663e7cbfc987c9c09c16de204e701
Zoe <hi@zoe.im>
2021-10-18 17:29:33 +0800

    :tada: remove unused fields to example site

--- src/plugins/sitemeta.js
+++ src/plugins/sitemeta.js
@@ -23,13 +23,49 @@ type Nav {
     items: [Nav]
 }
 
+type Link {
+    title: String
+    category: String
+    href: String
+}
+
+type Release {
+    provider: String
+    repo: String!
+}
+
+type Project {
+    provider: String
+    tag: String
+    owners: [String]
+}
+
+type Helpqa {
+    provider: String
+    repo: String
+    labelPrefix: String
+}
+
+type Comment {
+    provider: String
+    repo: String
+    repoID: String
+    category: String
+    categoryID: String
+    theme: String
+}
+
 type SiteSiteMetadata implements Node {
 
     title: String!
-
     author: Author
-
     navs: [Nav]
+    links: [Link]
+    socials: Author
+    releases: [Release]
+    projects: [Project]
+    helpqa: [Helpqa]
+    comments: Comment
 
 }`;
 
