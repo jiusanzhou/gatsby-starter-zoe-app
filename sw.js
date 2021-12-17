5a091a05f4b229f01fb65bc723947f3ee9bbe310
jiusanzhou <hi@zoe.im>
2021-12-17 07:04:00 +0000

    jiusanzhou published a site update

--- sw.js
+++ sw.js
@@ -48,11 +48,11 @@ self.__precacheManifest = [
     "url": "9f96d65d-43b2df01ca5c9b101ae3.js"
   },
   {
-    "url": "app-905beab7e7b4674f6a0e.js"
+    "url": "app-50f4485c19e55fea076b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e7d7d4bae19b20ee90ed2229ac7fec28"
+    "revision": "8eabdc8067f5451c0eb6754bcde87401"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a12c34d8f47c6528d910.js"
@@ -71,7 +71,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/1730835263.json",
-    "revision": "c597c042375e4f94da9e9ad9810d5d1d"
+    "revision": "667500e91688b740420006ad354af4f2"
   },
   {
     "url": "page-data/sq/d/3230166491.json",
@@ -79,15 +79,15 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/3912480317.json",
-    "revision": "1f91308f5f6fce8c07d6713c25cd3595"
+    "revision": "31eb314a23d002ecf9a5295e398d0720"
   },
   {
     "url": "page-data/sq/d/4276287135.json",
-    "revision": "ad080130bd1772ed49da0504e20b659b"
+    "revision": "8e17b305727e67d3fd16364bc96e9e11"
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c55e2c65336f1ef749ad739c3050d65b"
+    "revision": "a6a91d2cadfd324a3ab1aac75aedcfdb"
   },
   {
     "url": "polyfill-7644dbc8b87bbf2ba314.js"
@@ -186,7 +186,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/gatsby-starter-zoe-app/app-905beab7e7b4674f6a0e.js`))) {
+  if (!resources || !(await caches.match(`/gatsby-starter-zoe-app/app-50f4485c19e55fea076b.js`))) {
     return await fetch(event.request)
   }
 
