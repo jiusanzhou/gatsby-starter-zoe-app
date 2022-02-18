d165e33f07e007fb78e2e190d47f82ae98fefaac
Zoe <hi@zoe.im>
2022-02-18 10:17:10 +0800

    fea: init

---
+++ src/plugins/though-words.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+
+// data from Twitter or Weibo
+
+class Provider {
+
+}
+
+// https://weibo.com/ajax/statuses/mymblog?uid=1742820792&page=1&feature=0
+// https://weibo.com/ajax/profile/info?custom=jsz3
+class WeiboProvider {
+    constructor() {
+        
+    }
+}
+
+class TwitterProvider {}
---
+++ src/widgets/features.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FeaturesList = () => {
+
+}
+
+const FeaturesGrid = () => {
+
+}
+
+const FeaturesWrapper = () => {
+
+}
+
+export default FeaturesWrapper;
